3b152ffe31a1fc7226065d1d60a66d84ec18a4d7
jasonlee328 <jasonl@allenai.org>
2025-10-25 15:58:36 -0700

    robotiq push

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/push/config/franka_robotiq_2f85/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/push/config/franka_robotiq_2f85/__init__.py
@@ -51,3 +51,14 @@ gym.register(
     },
     disable_env_checker=True,
 )
+
+
+gym.register(
+    id="Blocks-Robotiq2f85-CustomOmni-Push",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    kwargs={
+        "env_cfg_entry_point": robotiq2f85_joint_rel_env_cfg.FrankaRobotiq2f85CustomOmniPushEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:Base_PPORunnerCfg",
+    },
+    disable_env_checker=True,
+)
